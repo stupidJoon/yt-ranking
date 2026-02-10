44cee5607cc5133ee0e3631a620dd8ea0ee9fee8
stupidJoon <junsang.yu3@gmail.com>
2026-02-10 06:40:15 +0000

    modify: enhance country redirection logic

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,7 +1,11 @@
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { awsCountries } from '@/lib/country.ts';
 
 export default async function Root() {
-  const country = (await headers()).get('x-vercel-ip-country') || 'US';
-  redirect(`/${country.toLowerCase()}`);
+  const country = (await headers()).get('x-vercel-ip-country') ?? 'US';
+  if (awsCountries.some((c) => c.code === country)) {
+    redirect(`/${country.toLowerCase()}`);
+  }
+  else redirect('/us');
 }
